ccce72f65d235f5180c88016e75c47c7ec6ef9ed
Dmitry Zasenko <dzasenko@c3r5p3.42vienna.com>
2025-06-20 11:16:37 +0200

    ex00 final

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,67 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzasenko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/20 11:06:28 by dzasenko          #+#    #+#             */
+/*   Updated: 2025/06/20 11:06:31 by dzasenko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
-#include <stdio.h>
+#include <string>
+#include <algorithm>
 
-void ft_strupcase(char *str)
+unsigned char make_up(unsigned char c)
 {
-    // if (!str)
-    //     exit(1);
-	while (*str)
-	{
-		if (*str >= 'a' && *str <= 'z')
-		{
-			*str = *str - 32;
-		}
-        std::cout << *str;
-		str++;
-	}
+    return std::toupper(c);
 }
 
-// int main(int ac, char *av[])
-int main(void)
+int main(int ac, char *av[])
 {
-    using namespace std;
-    // cout << "\a Operation \" Hyper Hype \" is now activated ! \n";
-    // cout << "Enter your agent code:__________\b\b\b\b\b\b\b\b\b";
-    // long code;
-    // cin >> code;
-    // cout << "\aYou entered [" << code << "] ...\n";
-    // cout << "\aCode ver i fied ! Proceed with Plan ZЗ!\n";
+    if (ac < 2)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 0;
+    }
 
-    cout.setf(ios_base::fixed, ios_base::floatfield);
-    float tub = 10.0 / 3.0;
-    double mint = 10.0 / 3.0;
-    const float million = 1.0e6;
-    cout << " tub = " << tub;
-    cout << ", а million tubs = " << million * tub;
-    cout << ",\nand ten million tubs = ";
-    cout << 10 * million * tub << endl;
-    cout << "mint = " << mint << " and а mill ion mints = ";
-    cout << million * mint << endl;
+    int i = 1;
+    while(av[i])
+    {
+        std::string s = av[i];
+        size_t start = s.find_first_not_of(" \t");
+        size_t finish = s.find_last_not_of(" \t");
+        if (start == std::string::npos || finish == std::string::npos || start > finish)
+        {
+            i++;
+            continue;
+        }
+        s = s.substr(start, finish - start + 1);
+        std::transform(s.begin(), s.end(), s.begin(), make_up);
+        std::cout << s;
+        if (i != ac - 1)
+            std::cout << " ";
+        i++;
+    }
+    std::cout << std::endl;
     return 0;
-
-    // char alarm = 7;
-    // std::cout << alarm << "Don't do that again !\a\n";
-
-    // char c;
-    // std::cout << "Enter CHAR:" << std::endl;
-    // std::cin >> c;
-    // std::cout << "[" << c << "]" << std::endl;
-    // std::cout.put('f');
-
-    // if (ac < 2)
-    //     std::cout
-    //         << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-
-    // int i = 1;
-    // while(av[i])
-    // {
-    //     ft_strupcase(av[i]);
-    //     if (i++ < ac)
-    //         std::cout << " ";
-    // }
-    // std::cout << std::endl;
-
-    // return 0;
 }
-//c++ -Wall -Wextra -Werror -std=c++98 megaphone.cpp
-//
